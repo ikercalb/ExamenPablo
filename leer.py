0281c657359f130ee42d095a09a717644824ed03
ikercalb <ikercaballero271@gmail.com>
2023-09-29 10:12:27 +0200

    cambios en la clase y en la funcion leer

--- leer.py
+++ leer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 from vuelo import Vuelo
 
-
-
+vuelos = []
 
 
 def leer():
-   
+        vuelos = Vuelo.cargar_array()
+        if vuelos!= None:
+            for v in vuelos:
+                print(v)
+    
+
