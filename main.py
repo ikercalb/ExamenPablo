3a5d3cf12d5ffdcc96cb14570e649f677ce50738
44jon44 <jonmadi21@gmail.com>
2023-09-29 09:53:04 +0200

    borrar funciona1

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3  
 import re
 from datetime import datetime
+from vuelo import Vuelo
+from vuelo import array_vuelos
+import borrar
 import json
 import os
 
-def cargar_array1():
-    with open("datos.json", "r") as archivo:
-        try:
-            diccionario = json.load(archivo)
-        except(json.decoder.JSONDecodeError):
-            print("no hay datos")
-            diccionario={}
-        for d in diccionario:
-            p1 = Persona(d.get("dni"),d.get("nombre"),d.get("edad"),d.get("fechaNac"))
-            personas.append(p1)
-def guardarJson2(personas):
-    personas=[p.aDiccionario()for p in personas]
-    archivo=open("datos.json","w")
-    data=json.dumps(personas)
-    archivo.write(data)
-    archivo.close
+
+archivoJson = "vuelos.json"
+
+# Comprueba si el archivo JSON ya existe
+if not os.path.exists(archivoJson):
+    # El archivo no existe, así que lo creamos
+    with open(archivoJson, "w") as nuevo_archivo_json:
+        # No escribimos ningún contenido en el archivo en este caso
+        pass
+    print("Archivo JSON creado. No hay datos.")
+else:
+    print("El archivo JSON ya existe, no se realizó ninguna escritura.")
+
+
 while True:
     modo = input("¿Qué modo deseas: crear, leer, actualizar o borrar(c r u d)? o 's' para salir: ")
         
--- vuelo.py
+++ vuelo.py
@@ -1,37 +1,51 @@
-#!/usr/bin/env python3import json
+#!/usr/bin/env python3
 import json
-vuelos = []
+import time
 
+array_vuelos = []
 
 class Vuelo:
-    def __init__(self,id_vuelo,destino_vuelo,fecha_vuelo,plazas_vuelo):
-        self.id_vuelo=id_vuelo
-        self.destino_vuelo=destino_vuelo
-        self.fecha_vuelo=fecha_vuelo
-        self.plazas_vuelo=plazas_vuelo
-    def toDiccionario(self):
-        return{
-            "id_vuelo":self.vuelo,
-            "destino_vuelo":self.destino_vuelo,
-            "fecha_vuelo":self.fecha_vuelo,
-            "plazas_vuelo":self.plazas_vuelo
-        }
+    
+    def __init__(self, id_vuelo, destino_vuelo, fecha_vuelo, plaza_vuelo):
+        self.id_vuelo = id_vuelo
+        self.fecha_vuelo = fecha_vuelo
+        
+        self.destino_vuelo = destino_vuelo  
+        self.plaza_vuelo = plaza_vuelo
 
+    def crear_vuelo(destino_vuelo, fecha_vuelo, plaza_vuelo):
+        id_vuelo = Vuelo.generar_id_vuelo()
+        vuelo = Vuelo(id_vuelo, destino_vuelo, fecha_vuelo, plaza_vuelo)
+        return vuelo
+
+    def generar_id_vuelo():  
+        current_time = int(time.time())
+        return f"Vuelo_{current_time}"
+        
+    def to_diccionario(self):
+        return {
+            "id_vuelo": self.id_vuelo,
+            "fecha_vuelo": self.fecha_vuelo,
+            "destino_vuelo": self.destino_vuelo,
+            
+            "plaza_vuelo": self.plaza_vuelo
+        }
+        
     def cargar_array():
-        vuelos.clear()
+        array_vuelos.clear()
         try:
             with open("vuelos.json", "r") as archivo:
                 diccionario = json.load(archivo)
                 for d in diccionario:
-                    v1 = Vuelo(d.get("fecha_vuelo"),d.get("id_vuelo"),d.get("destino_vuelo"),d.get("plaza_vuelo"))
-                    vuelos.append(v1)
-                return(vuelos)
+                    v1 = Vuelo(d.get("id_vuelo"),d.get("fecha_vuelo"),d.get("destino_vuelo"),d.get("plaza_vuelo"))
+                    array_vuelos.append(v1)
+                return(array_vuelos)
         except:
             print("No hay vuelos guardados")
 
     def cargar_json(array_vuelos):
-        personas_dict = [v.to_dict() for v in array_vuelos]
+        vuelos_dict = [v.to_dict() for v in array_vuelos]
         text_file = open("vuelos.json", "w")
-        json_data = json.dumps(personas_dict)
+        json_data = json.dumps(vuelos_dict)
         text_file.write(json_data)
         text_file.close()
