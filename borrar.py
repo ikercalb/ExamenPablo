ede9ac14e726e6e1ff1ef0845030bff2cae84854
44jon44 <jonmadi21@gmail.com>
2023-09-29 08:47:43 +0200

    ClaseBorrar

--- borrar.py
+++ borrar.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-from clase import Vuelo
-
-
-
-
+from vuelo import Vuelo
+from vuelo import vuelos
 
 def borrar():
-   
+    encontrado=False
+    Vuelo.cargar_array
+    idV=input("Introduce el id del vuelo que seas borrar:")
+    for p in vuelos:
+        if p.id_vuelo==idV:
+            vuelos.remove(p)
+            encontrado=True
+    if encontrado==False:
+        print("No se ha encontrado ningun vuelo con ese id")
+    Vuelo.cargar_json
 
 
